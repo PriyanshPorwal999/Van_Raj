e5f9ccfada2d1d4d92bd06820491e97629a59825
singhishant37@gmail.com <singhishant37@gmail.com>
2025-09-26 14:33:49 +0530

    Added the DSS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import FRAAtlas from './components/FRAAtlas';
+// import FRAAtlas from './components/FRAAtlas';
 import './App.css';
+import { DecisionSupportPanel } from './components/Decision-support-panel';
 
 function App() {
   return (
     <div className="App">
-      <FRAAtlas />
+      <DecisionSupportPanel />
     </div>
   );
 }
